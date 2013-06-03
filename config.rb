1dfdde90ac2af2099c153e88cbaa9271b8059586
Facundo Ferrero <facundo.a.ferrero@accenture.com>
2013-06-03 16:39:24 -0300

    Actualización con lo enviado a UAT
    SE CAMBIARON MUCHOS PATH DE IMAGENES Y RECURSOS PARA QUE SEAN
    COMPATIBLES CON VCM

--- config.rb
+++ config.rb
@@ -1,5 +1,3 @@
-#require 'ZURB-foundation'
-#require 'rgbapng'
 # Require any additional compass plugins here.
 
 
@@ -12,14 +10,13 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-#line_comments = false
-
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
